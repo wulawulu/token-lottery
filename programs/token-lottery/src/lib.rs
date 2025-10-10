e63dcb196cb7e96bf7788a82a1e157d9597d59c5
wulawulu <wutuobangbang@gmail.com>
2025-10-10 16:57:40 +0800

    feat: init token lottery

--- programs/token-lottery/src/lib.rs
+++ programs/token-lottery/src/lib.rs
@@ -1,16 +1,52 @@
 use anchor_lang::prelude::*;
 
-declare_id!("HAsY3haak8k3TnvXhE2pZgVigvc1QBx4JF2Eixm2yJYV");
+declare_id!("xvHHLbBKiiGusjRzrmAcYT7C72qYpH2h69uh9a9kRW4");
 
 #[program]
 pub mod token_lottery {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        msg!("Greetings from: {:?}", ctx.program_id);
+    pub fn initialize_config(ctx: Context<InitializeConfig>, start_time: u64, end_time: u64, price: u64) -> Result<()> {
+        ctx.accounts.token_lottery.bump = ctx.bumps.token_lottery;
+        ctx.accounts.token_lottery.start_time = start_time;
+        ctx.accounts.token_lottery.end_time = end_time;
+        ctx.accounts.token_lottery.price = price;
+        ctx.accounts.token_lottery.authority = ctx.accounts.payer.key();
+        ctx.accounts.token_lottery.lottery_pot_amount = 0;
+        ctx.accounts.token_lottery.total_ticket = 0;
+        ctx.accounts.token_lottery.randomness_account = Pubkey::default();
+        ctx.accounts.token_lottery.winner_chosen = false;
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct InitializeConfig<'info> {
+    #[account(mut)]
+    pub payer: Signer<'info>,
+
+    #[account(
+        init,
+        payer = payer,
+        space = 8 + TokenLottery::INIT_SPACE,
+        seeds = [b"token_lottery".as_ref()],
+        bump
+    )]
+    pub token_lottery: Account<'info, TokenLottery>,
+    pub system_program: Program<'info, System>,
+}
+
+#[account]
+#[derive(InitSpace)]
+pub struct TokenLottery {
+    pub bump: u8,
+    pub winner: u64,
+    pub winner_chosen: bool,
+    pub start_time: u64,
+    pub end_time: u64,
+    pub lottery_pot_amount: u64,
+    pub total_ticket: u64,
+    pub price: u64,
+    pub authority: Pubkey,
+    pub randomness_account: Pubkey,
+}
--- tests/token-lottery.ts
+++ tests/token-lottery.ts
@@ -4,13 +4,30 @@ import { TokenLottery } from "../target/types/token_lottery";
 
 describe("token-lottery", () => {
   // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.AnchorProvider.env());
+  const provider = anchor.AnchorProvider.env();
+  anchor.setProvider(provider);
+  const wallet = provider.wallet as anchor.Wallet;
 
   const program = anchor.workspace.tokenLottery as Program<TokenLottery>;
 
-  it("Is initialized!", async () => {
+  it("should init config", async () => {
     // Add your test here.
-    const tx = await program.methods.initialize().rpc();
+    const initConfigIx = await program.methods.initializeConfig(
+      new anchor.BN(10), 
+      new anchor.BN(1860085765), 
+      new anchor.BN(10000) 
+    ).instruction();
+    const blockhashWithContext = await provider.connection.getLatestBlockhash();
+
+    const tx = new anchor.web3.Transaction({
+      feePayer: wallet.publicKey,
+      blockhash: blockhashWithContext.blockhash,
+      lastValidBlockHeight: blockhashWithContext.lastValidBlockHeight,
+    })
+    .add(initConfigIx);
     console.log("Your transaction signature", tx);
+
+    const signature = await anchor.web3.sendAndConfirmTransaction(provider.connection, tx, [wallet.payer]);
+    console.log("Your transaction signature", signature);
   });
 });
